5534e66809175157eae4248a8826fa3ad46546bf
AbhilashJN <abhilashjn107@gmail.com>
2018-01-24 12:56:44 +0530

    test case for empty file and 1 \n passed

--- firstio.test.js
+++ firstio.test.js
@@ -9,6 +9,14 @@ describe('testing the readString function' , ()=> {
 	});
     
 
+	test('Test file with 1 \n, should return 1' , ()=>{
 
+		expect(readString('sample2.txt')).toBe(1);
+	});
+    
+	test('Test empty file should return 0' , ()=>{
+
+		expect(readString('sample3.txt')).toBe(0);
+	});
     
 });
