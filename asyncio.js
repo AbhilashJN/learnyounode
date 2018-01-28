c6e3b5aa0eece6a436be8f715a10cb2a62a12799
AbhilashJN <abhilashjn107@gmail.com>
2018-01-28 12:07:23 +0530

    4 test cases passed

---
+++ asyncio.js
@@ -0,0 +1,26 @@
+
+function readString(filePath)
+{
+	
+	let fs = require('fs');
+
+	let bufStringArr = fs.readFile(filePath,'utf8' ,(err, data) => {
+		
+		if (err) throw err;
+		data = data.split('\n');
+		console.log (data.length-1);
+		
+	
+	});
+	
+}
+
+
+
+
+readString(process.argv[2]);
+
+
+
+module.exports = readString;
+
---
+++ asyncio.test.js
@@ -0,0 +1,30 @@
+let readString = require('./asyncio');
+
+
+
+
+
+describe('testing the readString function' , ()=> {
+
+	test('Test file with 0 \n, should return 0' , ()=>{
+		console.log = jest.fn();
+		
+		expect(readString('sample.txt')).toBe(0);
+	});
+    
+
+	test('Test file with 1 \n, should return 1' , ()=>{
+
+		expect(readString('sample2.txt')).toBe(1);
+	});
+    
+	test('Test empty file should return 0' , ()=>{
+
+		expect(readString('sample3.txt')).toBe(0);
+	});
+	test('Test file with 5 \n, should return 5' , ()=>{
+
+		expect(readString('sample4.txt')).toBe(5);
+	});
+    
+});
