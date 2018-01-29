887e0a1a0362fe93c6c76810a8301ba40ee192eb
AbhilashJN <abhilashjn107@gmail.com>
2018-01-30 02:05:28 +0530

    changed response link

--- httpclient.test.js
+++ httpclient.test.js
@@ -3,16 +3,16 @@ let httpclient = require('./httpclient');
 describe('Testing for data in response', () => {
 	test('Testing for valid HTTP response', (done) => {
 		let callback = (err,data) => {
-			expect(data).toMatch('200 OK');
+			expect(data).toMatch('123456789');
 			done();
 		};
-		httpclient('http://httpstat.us/200', callback);
+		httpclient('http://www.mocky.io/v2/5a6f83af300000e6336fe63b', callback);
 	});
 	test('Testing for failed HTTP response', (done) => {
 		let callback = (err,data) => {
 			expect(data).toMatch('404 Not Found');
 			done();
 		};
-		httpclient('http://httpstat.us/404', callback);
+		httpclient('http://www.mocky.io/v2/5a6f852f300000e6336fe640', callback);
 	});
 });
