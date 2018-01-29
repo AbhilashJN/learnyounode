8185f74dd5a4a4d9fdb7a1eae77e5e064cc41ec6
AbhilashJN <abhilashjn107@gmail.com>
2018-01-30 00:08:33 +0530

    asyncio.js: added test case to check for invalid file

--- asyncio.js
+++ asyncio.js
@@ -1,24 +1,24 @@
 
-function readString(filePath)
+function readString(filePath,callback)
 {
 	
 	let fs = require('fs');
 
-	let bufStringArr = fs.readFile(filePath,'utf8' ,(err, data) => {
-		
-		if (err) throw err;
-		data = data.split('\n');
-		console.log (data.length-1);
-		done();
-	
-	});
+	let bufStringArr = fs.readFile(filePath,'utf8' ,callback);
 	
 }
 
 
+function callback(err, data){
+		
+	if (err) throw err;
+	data = data.split('\n');
+	console.log (data.length-1);
+	return data.length-1;
 
+}
 
-readString(process.argv[2]);
+//readString(process.argv[2],callback);
 
 
 
--- asyncio.test.js
+++ asyncio.test.js
@@ -6,25 +6,56 @@ let readString = require('./asyncio');
 
 describe('testing the readString function' , ()=> {
 
-	test('Test file with 0 \n, should return 0' , ()=>{
-		console.log = jest.fn();
-		readString('sample.txt');
-		expect(console.log).toHaveBeenCalledWith(0);
+	test('Test file with 0 \n, should return 0' , (done)=>{
+		function callback(err,data){
+			if (err) throw err;
+			data = data.split('\n');
+			expect(data.length-1).toBe(0);
+			done();
+		} 
+		readString('sample.txt',callback);
 	});
     
 
-	test('Test file with 1 \n, should return 1' , ()=>{
+	test('Test file with 1 \n, should return 1' , (done)=>{
+		function callback(err,data){
+			if (err) throw err;
+			data = data.split('\n');
+			expect(data.length-1).toBe(1);
+			done();
+		} 
+		readString('sample2.txt',callback);
 
-		expect(readString('sample2.txt')).toBe(1);
 	});
     
-	test('Test empty file should return 0' , ()=>{
-
-		expect(readString('sample3.txt')).toBe(0);
+	test('Test empty file should return 0' , (done)=>{
+		function callback(err,data){
+			if (err) throw err;
+			data = data.split('\n');
+			expect(data.length-1).toBe(0);
+			done();
+		} 
+		readString('sample3.txt',callback);
 	});
-	test('Test file with 5 \n, should return 5' , ()=>{
+	test('Test file with 5 \n, should return 5' , (done)=>{
 
-		expect(readString('sample4.txt')).toBe(5);
+		function callback(err,data){
+			if (err) throw err;
+			data = data.split('\n');
+			expect(data.length-1).toBe(5);
+			done();
+		} 
+		readString('sample4.txt',callback);
 	});
+	test('Test filename which does not exist, should throw error' , (done)=>{
+
+		function callback(err,data){
+			console.log(err);
+			expect(err.code).toEqual('ENOENT');
+			done();
+		} 
+		readString('a.txt',callback);
+	});
+
     
 });
